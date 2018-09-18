cfbfa419bf86d64666356b3e9fd51fafb8b4472b
Tao Olocan <81315@b5-420-02.ict.lab.locals>
2018-09-18 12:02:47 +0200

    firstcommit

---
+++ Website/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main.css">
+		<meta charset="UTF-8">
+		<title>Untitled Document</title>
+		
+	</head>
+
+	<body>
+		
+		<header>
+		
+			<nav>
+				<ul>
+					<li><a href="*">Lorem ipsum</a></li>
+					<li><a href="*">sit amet</a></li> 
+					<li><a href="*">consectetur</a></li>
+					<li><a href="*">gometor</a></li>
+				</ul>   
+			</nav>
+		
+		</header>
+		
+		<main>
+		
+		
+		
+		</main>
+		
+		<footer>
+		
+		
+		
+		</footer>
+		
+		
+	</body>
+</html>
